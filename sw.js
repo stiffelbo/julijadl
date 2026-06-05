f40162793817542e5533213a2600b447d48f298c
stiffelbo <partygrouppl@gmail.com>
2026-06-05 21:07:26 +0200

    cache change

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="baby-tracker-pwa-v1";
+const CACHE_NAME="baby-tracker-pwa-v2";
 const ASSETS=["./","./index.html","./app.js","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",(event)=>{
